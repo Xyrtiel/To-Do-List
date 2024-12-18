4525aeb03a934354cf201dddd0c0918af025eecd
Xyrtiel <147344087+Xyrtiel@users.noreply.github.com>
2024-12-18 16:55:34 +0100

    initialisation_project_template

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue dans React!</h1>
+      <p>Ceci est un fichier App.js minimal pour tester votre application.</p>
+    </div>
+  );
+}
+
+export default App;
